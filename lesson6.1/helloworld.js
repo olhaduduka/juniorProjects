50020d0ffe135e40aaa2abbdbf6c14545e337b45
dudukaolga <dudukaolga@gmail.com>
2017-03-04 13:36:03 +0100

    Playing with figures

---
+++ lesson6.1/helloworld.js
@@ -0,0 +1,21 @@
+var diamond = document.getElementById("diamond"); // red
+var circle = document.getElementsByClassName('circle'); // green border
+var his = document.getElementsByTagName("h1"); // 50 font-size
+
+
+diamond.className = "diamond red";
+his[0].className = 'hi';
+
+
+// var rectangles = document.getElementsByTagName('');
+// for (var i=0; i<rectangles.length; i++) {
+//   rectangles[i].style.width = 50;
+// }
+//
+// var newDiv = document.createElement('div');
+// newDiv.className = 'rectangle inline';
+// var wrapper = document.getElementById('wrapper-new-rectangle');
+// wrapper.appendChild(newDiv);
+//
+// var div2 = newDiv.cloneNode();
+// wrapper.appendChild(div2);
